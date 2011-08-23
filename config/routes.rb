8174335bb220d7512a07358c0631622da31cf46d
Charlie Siegel <charlie.siegel@gmail.com>
2011-08-23 19:29:28 -0400

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
+  match '/contact', :to => 'pages#contact'
+  match '/about',	:to => 'pages#about'
+  match '/help',	:to => 'pages#help'
   
-  get "pages/about"
-  
-  get "pages/help"
+  root :to => "welcome#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -63,4 +61,5 @@ SampleApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+
 end
--- shuffle_join.rb
+++
@@ -1,6 +0,0 @@
-class String
-	def shuffle
-		self.split('').to_a.shuffle.join
-	end
-end
-
